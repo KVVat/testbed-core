d438e767d595f6939aa7822002e2877a2d884d10
kwatanabe <kkww413@gmail.com>
2026-02-07 06:09:19 +0900

    Add Dependency Check

---
+++ composeApp/src/jvmMain/kotlin/org/example/project/test/SampleTest.kt
@@ -0,0 +1,30 @@
+package org.example.project.test
+
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class FCS_CKH_EXT1_HIGH {
+
+    @Before
+    fun setUp()
+    {
+        runBlocking {
+
+        }
+    }
+    @After
+    fun teardown() {
+        runBlocking {
+
+        }
+    }
+
+    @Test
+    fun testOutput() {
+        runBlocking{
+        }
+        //logging("ClassName"+this.javaClass.canonicalName);
+    }
+}
